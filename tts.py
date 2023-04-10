803ee5aa9a6a24e6ff097d0b91d04e754afd6eb9
Ic-0n0111 <113190401+Ic-0n011@users.noreply.github.com>
2023-04-10 17:52:01 +0300

    Add files via upload

---
+++ tts.py
@@ -0,0 +1,23 @@
+import torch
+import sounddevice as sd
+
+text = 'приветствую! ... я ... голосовой помощник Айкон!'
+language = 'ru'
+model_id = 'v3_1_ru'
+sample_rate = 48000
+speaker = 'xenia' # aidar, baya, kseniya, xenia, random
+device = torch.device('cpu')
+model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_tts', language=language, speaker=model_id)
+model.to(device)
+
+
+# воспроизводим
+def va_speak(what: str):
+    audio = model.apply_tts(text=what, speaker=speaker, sample_rate=sample_rate)
+    sd.play(audio, sample_rate)
+    sd.wait()
+
+va_speak(text)
+# sd.play(audio, sample_rate)
+# time.sleep(len(audio) / sample_rate)
+# sd.stop()
